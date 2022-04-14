452e2d490c9d0657a949004222b7909d06cb6bb0
gt <jack139@gmail.com>
2022-04-14 11:06:54 +0800

    update

--- locate/cnn_train.py
+++ locate/cnn_train.py
@@ -35,8 +35,8 @@ val_generator = dataGenerator(val_dir, val_json, batch_size=batch_size, target_s
 
 
 # 生成模型
-#model = get_model(model_type, input_size=input_size, freeze=freeze)
-model = get_model(model_type, input_size=input_size, freeze=True, weights=None) # for test
+model = get_model(model_type, input_size=input_size, freeze=freeze)
+#model = get_model(model_type, input_size=input_size, freeze=True, weights=None) # for test
 
 model.compile(loss=IoULoss, optimizer=Adam(lr=learning_rate), metrics=[IoU, IoU2])
 
